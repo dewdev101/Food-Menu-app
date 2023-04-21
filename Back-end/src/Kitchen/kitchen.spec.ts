d1f1d9045f7a97ce0829ce657d37f64efa952095
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-04-21 19:43:15 +0700

    create updateOrder unit test

--- Back-end/src/Kitchen/kitchen.spec.ts
+++ Back-end/src/Kitchen/kitchen.spec.ts
@@ -1,61 +1,61 @@
 import { addCategory, addMenu, createOrder, deleteMenu, updateOrders } from "./resolve";
 
-describe("addCategory", () => {
-  test("should add a category", async () => {
-    const category = "ทอด";
-    const result = await addCategory({
-      category: category,
-    });
-    console.log("result", result);
-    expect(result.category.length > 0).toBe(true);
-  });
-});
+// describe("addCategory", () => {
+//   test("should add a category", async () => {
+//     const category = "ทอด";
+//     const result = await addCategory({
+//       category: category,
+//     });
+//     console.log("result", result);
+//     expect(result.category.length > 0).toBe(true);
+//   });
+// });
 
-describe("addMenu", () => {
-  test("should add menu", async () => {
-    const menu = {
-      name: "ยอดฟักแม้วผัดหมูสับ",
-      image: "https://i.ytimg.com/vi/6yM0oog1iDQ/maxresdefault.jpg",
-      price: 130,
-      categoryId: 3,
-    };
-    const result = await addMenu({
-      name: menu.name,
-      image: menu.image,
-      price: menu.price,
-      categoryId: menu.categoryId,
-    });
-    console.log("result", result);
-    expect(result.name.length > 0).toBe(true);
-  });
-});
+// describe("addMenu", () => {
+//   test("should add menu", async () => {
+//     const menu = {
+//       name: "ยอดฟักแม้วผัดหมูสับ",
+//       image: "https://i.ytimg.com/vi/6yM0oog1iDQ/maxresdefault.jpg",
+//       price: 130,
+//       categoryId: 3,
+//     };
+//     const result = await addMenu({
+//       name: menu.name,
+//       image: menu.image,
+//       price: menu.price,
+//       categoryId: menu.categoryId,
+//     });
+//     console.log("result", result);
+//     expect(result.name.length > 0).toBe(true);
+//   });
+// });
 
-describe("deleteMenu", () => {
-  test("deleteMenu", async () => {
-    const deleteId = 9;
-    const result = await deleteMenu({
-      id: deleteId,
-    });
-    console.log("result", result);
-    expect(result.count > 0).toBe(true);
-  });
-});
+// describe("deleteMenu", () => {
+//   test("deleteMenu", async () => {
+//     const deleteId = 9;
+//     const result = await deleteMenu({
+//       id: deleteId,
+//     });
+//     console.log("result", result);
+//     expect(result.count > 0).toBe(true);
+//   });
+// });
 
-describe("createOrder", () => {
-  test("create order", async () => {
-    const tableId = 11;
-    const items = [{ quantity: 4, totalPrice: 900, menuId: 5 }];
+// describe("createOrder", () => {
+//   test("create order", async () => {
+//     const tableId = 11;
+//     const items = [{ quantity: 4, totalPrice: 900, menuId: 5 }];
 
-    const result = await createOrder({
-      tableId: tableId,
-      items: items,
-    });
-    console.log("result", result);
-    expect(result.tableId > 0).toBe(true);
-    expect(result.id > 0).toBe(true);
-    expect(result.status.length > 0).toBe(true);
-  });
-});
+//     const result = await createOrder({
+//       tableId: tableId,
+//       items: items,
+//     });
+//     console.log("result", result);
+//     expect(result.tableId > 0).toBe(true);
+//     expect(result.id > 0).toBe(true);
+//     expect(result.status.length > 0).toBe(true);
+//   });
+// });
 
 describe("updateOrder", () => {
   test("update order", async() => {
